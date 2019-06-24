e133a0b12065b2ac660e790be216e001b454ddab
Jared Matthew P. De Guzman <jartalk@gmail.com>
2019-06-24 11:27:10 +0800

    Added extra comments

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ import {html,render} from './node_modules/lit-html/lit-html.js';
 
 //const webItemList = document.getElementById("item-list")
 const listAppearingOnSite = document.getElementById("item-list")
-var webItemList = [];
+var webItemList = []
 //const myTemplate = (todo) => html` <li>${todo}</li>`
 var todoItems
 
 if(localStorage.getItem("tasks")){
   todoItems = JSON.parse(localStorage.getItem("tasks"))
 } else {
-  todoItems = [];
+  todoItems = []
 }
 
 //Print existing tasks from previous sessions
@@ -29,7 +29,7 @@ document.getElementById("add-button").addEventListener("click", function() {
   addItemToFrontEnd(item)
 });
 
-//Fix not updating instantly
+
 document.getElementById("delete-button").addEventListener("click", function() {
   todoItems = []
   localStorage.setItem("tasks", JSON.stringify(todoItems))
@@ -41,6 +41,10 @@ function addItemToFrontEnd(item){
   //Insert HTML template here soon
   var deleteButton = document.createElement("button")
   deleteButton.innerHTML = "Delete"
+
+  //Change id attribute of <li> in general so edit and delete can use the same point of reference.
+  //Reference certain task ID using .parent() function of the deleteButton (Similar to the function deleteItem)
+
   deleteButton.setAttribute("id", todoItems.indexOf(item))
 
   deleteButton.addEventListener("click", function() {
@@ -49,10 +53,11 @@ function addItemToFrontEnd(item){
     localStorage.setItem("tasks", JSON.stringify(todoItems))
   });
 
+  //Make template cleaner
   webItemList.push(html`<li>${item}${deleteButton}</li>`)
   render(webItemList, listAppearingOnSite)
 }
 
-function deleteItem(){
-  console.log(this)
+function editItem() {
+
 }
